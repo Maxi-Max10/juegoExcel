90e28f3ca682bea6d0755829ebabe8db5ea3a7a0
Maxi-Max10 <maximilianoalderete017@gmail.com>
2026-04-01 10:33:06 -0300

    feat: Añadir script de Google Ads en la cabecera del layout

--- includes/layout.php
+++ includes/layout.php
@@ -18,6 +18,8 @@ function render_head(string $title): void
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="stylesheet" href="assets/css/styles.css?v=<?= filemtime(__DIR__ . '/../assets/css/styles.css') ?>">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3986035282338746"
+     crossorigin="anonymous"></script>
     <?php
 }
 
